4bf8e9e710cba402276325e67b9c74c63a3452f6
MercenaryHigh20 <mercenaryhigh@gmail.com>
2026-06-12 23:45:09 +0530

    Add files via upload

---
+++ toss/Toss.java
@@ -0,0 +1,11 @@
+public class Toss
+{
+    public static void main()
+    {
+        int n=(int)(Math.random()*2);
+        if(n==1)
+        System.out.println("IT'S A...HEADS!!!");
+        else
+        System.out.println("IT'S A...TAILS!!!");
+    }
+}
